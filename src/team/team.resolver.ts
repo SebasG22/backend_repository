5ee86b6ef3de25447cfadb64901e263e1842b138
Andrés Castillo <andres.castillo@acastillo.co>
2022-11-15 15:45:43 -0500

    add query for get a team by id

--- src/team/team.resolver.ts
+++ src/team/team.resolver.ts
@@ -12,6 +12,11 @@ export class TeamResolver {
         return this.teamService.getTeams();
     }
 
+    @Query()
+    getTeamById(@Args("teamId") teamId: string): Team {
+        return this.teamService.getTeamById(teamId);
+    }
+
     @Mutation()
     createTeam(@Args("name") name: string, @Args("flag_icon") flag_icon: string, @Args("background") background: string): Team {
         return this.teamService.createTeam(name, flag_icon, background)
